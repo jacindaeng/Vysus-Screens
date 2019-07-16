4a4df9c00697d0807e78f1c5e1d322c9fc4a16d7
Jacinda Eng <jceng@uw.edu>
2019-07-16 11:53:07 -0700

    Changes to welcome screen

--- Vysus Welcome version 3/Vysus Welcome version 3/ViewController.swift
+++ Vysus Welcome version 3/Vysus Welcome version 3/ViewController.swift
@@ -10,16 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var gifImageView: UIImageView!
     @IBOutlet var uiBackground: UIView!
     @IBOutlet weak var logoImageView: UIImageView!
-    @IBOutlet weak var animationImageView: UIImageView!
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
         uiBackground.backgroundColor = #colorLiteral(red: 0.7058823529, green: 0.4823529412, blue: 0.5490196078, alpha: 1)
-        logoImageView.image = UIImage(named: "Visus")
-        animateImage()
+        logoImageView.image = UIImage(na
+        
         // Do any additional setup after loading the view.
     }
     
