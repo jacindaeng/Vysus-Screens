e093f2499bddac0f818919767baa2957c7cfcb1f
Keneisha Wiggan <keneishawiggan8@gmail.com>
2019-07-16 11:49:50 -0700

    added a new view controller with its .swift connection

---
+++ Vysus Welcome version 3/Vysus Welcome version 3/AuthenticationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AuthenticationViewController.swift
+//  Vysus Welcome version 3
+//
+//  Created by Keneisha Wiggan on 7/16/19.
+//  Copyright © 2019 Jacinda Eng. All rights reserved.
+//
+
+import UIKit
+
+class AuthenticationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
